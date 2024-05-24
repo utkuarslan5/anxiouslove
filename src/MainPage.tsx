83aaf129484722ffeea2202166a97a5f47f0ac92
utkuarslan5 <utku.arslan@hotmail.com>
2024-05-24 15:29:48 +0200

    did smth very wrong, we'll see

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -4,10 +4,6 @@ import { useQuery, getTasks } from "wasp/client/operations";
 import { ChatBot } from "./ChatBot";
 
 export const MainPage: React.FC = () => {
-  // const { data: tasks, isLoading, error } = useQuery(getTasks);
-
-  // if (isLoading) return "Loading...";
-  // if (error) return "Error: " + error;
 
   return <ChatBot />;
 };
